80c4e0c07fadb6628bdb9e9fe54fa442fda93cf3
Minghua Guo <minghua.guo@bazaarvoice.com>
2014-07-11 16:47:36 -0500

    10071

---
+++ 10071.cpp
@@ -0,0 +1,17 @@
+/*--------------------
+
+UVa 10071 - Back to High School Physics
+mguo.ca
+
+--------------------*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	long i, j;
+	while(cin >> i >> j)
+		cout << i*j*2 << endl;
+	return 0;
+}
