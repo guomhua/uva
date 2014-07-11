93ae071bb0e5df0674bbe911c434474070905605
Minghua Guo <minghua.guo@bazaarvoice.com>
2014-07-11 16:38:44 -0500

    10055

---
+++ 10055.cpp
@@ -0,0 +1,17 @@
+/*--------------------
+
+UVa 10055 - Hashmat the brave warrior
+mguo.ca
+
+--------------------*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	long i, j;
+	while(cin >> i >> j)
+		cout << abs(j-i) << endl;
+	return 0;
+}
